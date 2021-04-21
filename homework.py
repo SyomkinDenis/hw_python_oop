97e2b72c3dbff6853b652f4bb22448e3c3f70eb3
Syomkin Denis <d5795@yandex.ru>
2021-04-22 00:40:19 +0300

    pep8 debug

--- homework.py
+++ homework.py
@@ -32,6 +32,8 @@ class Calculator:
 
     def get_today_stats(self):  # Считать, сколько денег\калорий потрачено сегодня
         today_records = [r for r in self.records if r.date == dt.date.today()]
+        for r in today_records:
+            print(r)
         today_sum = 0
         for r in today_records:
             today_sum += r.amount
@@ -40,13 +42,14 @@ class Calculator:
     def get_week_stats(self):  # Считать, сколько денег\калорий потрачено за последние 7 дней
         today = dt.date.today()
         seven_days_ago = today - dt.timedelta(days=7)
-        week_records = [r for r in self.records if r.date >= seven_days_ago]
+        week_records = [r for r in self.records if today >= r.date >= seven_days_ago]
         week_sum = 0
         for r in week_records:
             week_sum += r.amount
         return week_sum
 
     def _remain_limit(self):
+        print(self.limit, self.get_today_stats())
         return self.limit - self.get_today_stats()
 
 
@@ -62,12 +65,16 @@ class CaloriesCalculator(Calculator):
 class CashCalculator(Calculator):
     USD_RATE = 76.90
     EURO_RATE = 92.38
-    CURRENCY_RATE = {'rub': 1, 'eur': EURO_RATE, 'usd': USD_RATE}
     CURRENCY_NAME = {'rub': 'руб', 'eur': 'Euro', 'usd': 'USD'}
+
+    def get_rate(self, currency):
+        if currency != 'rub':
+            return getattr(self, f'{self.CURRENCY_NAME[currency].upper()}_RATE')
+        return 1
 
     def get_today_cash_remained(self, currency='rub'):
         remain_limit = self._remain_limit()
-        remain_limit_in_currency = round(remain_limit / self.CURRENCY_RATE[currency], 2)
+        remain_limit_in_currency = round(remain_limit / self.get_rate(currency), 2)
 
         if remain_limit_in_currency > 0:
             return f'На сегодня осталось {remain_limit_in_currency} {self.CURRENCY_NAME[currency]}'
@@ -75,13 +82,3 @@ class CashCalculator(Calculator):
             return f'Денег нет, держись: твой долг - {abs(remain_limit_in_currency)} {self.CURRENCY_NAME[currency]}'
         else:
             return 'Денег нет, держись'
-
-
-#cash_calculator = CashCalculator(1000)  # создадим калькулятор денег с дневным лимитом 1000
-
-#cash_calculator.add_record(Record(amount=145.05, comment='кофе'))
-#cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
-#cash_calculator.add_record(Record(amount=3000, comment='бар в Танин др', date='13.04.2021'))
-
-#print(cash_calculator.get_today_cash_remained('eur'))
-#print(cash_calculator.get_week_stats())
