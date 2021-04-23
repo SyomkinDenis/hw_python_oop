95603cc88ddf095e29d509fc5d8f9bb6881f3696
SyomkinDenis <77999003+SyomkinDenis@users.noreply.github.com>
2021-04-23 17:58:58 +0300

    fix issues by review

--- homework.py
+++ homework.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 
 class Record:
-    def __init__(self, amount, comment, date: Optional[str] = None):
+    def __init__(self, amount: int, comment: str, date: Optional[str] = None):
         date_format = '%d.%m.%Y'
         self.amount = amount
         if date is not None:
@@ -12,81 +12,65 @@ class Record:
             self.date = dt.date.today()
         self.comment = comment
 
-    def process_records(self):
-        record = [self.amount, self.date, self.comment]
-        print(record)
-        return record
-
     def __repr__(self):
         return f'{self.date}: {self.amount} ({self.comment})'
 
 
 class Calculator:
-    def __init__(self, limit):
+    def __init__(self, limit: int):
         self.limit = limit
         self.records = []
-        print(self.records)
 
     def add_record(self, record: Record):
         self.records.append(record)
 
     def get_today_stats(self):
-        today_records = [r for r in self.records if r.date == dt.date.today()]
-        for r in today_records:
-            print(r)
-        today_sum = 0
-        for r in today_records:
-            today_sum += r.amount
-        return today_sum
+        today = dt.date.today()
+        today_records = [r.amount for r in self.records if r.date == today]
+        return sum(today_records)
 
     def get_week_stats(self):
         today = dt.date.today()
         seven_days_ago = today - dt.timedelta(days=7)
-        week_records = [r for r in self.records if
+        week_records = [r.amount for r in self.records if
                         today >= r.date >= seven_days_ago]
-        week_sum = 0
-        for r in week_records:
-            week_sum += r.amount
-        return week_sum
+        return sum(week_records)
 
-    def _remain_limit(self):
-        print(self.limit, self.get_today_stats())
+    def remain_limit(self):
         return self.limit - self.get_today_stats()
 
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
-        remain_limit = self._remain_limit()
+        remain_limit = self.remain_limit()
         if remain_limit > 0:
             return('Сегодня можно съесть что-нибудь ещё, '
                    f'но с общей калорийностью не более {remain_limit} кКал')
-        else:
-            return 'Хватит есть!'
+        return 'Хватит есть!'
 
 
 class CashCalculator(Calculator):
     USD_RATE = 76.90
     EURO_RATE = 92.38
-    CURRENCY_NAME = {'rub': 'руб', 'eur': 'Euro', 'usd': 'USD'}
 
-    def get_rate(self, currency):
-        if currency != 'rub':
-            return(getattr(self,
-                           f'{self.CURRENCY_NAME[currency].upper()}_RATE'))
-        return 1
+    def get_currency(self, currency: str):
+        if currency == 'eur':
+            return self.EURO_RATE, 'Euro'
+        elif currency == 'usd':
+            return self.USD_RATE, 'USD'
+        return 1, 'руб'
 
-    def get_today_cash_remained(self, currency='rub'):
-        remain_limit = self._remain_limit()
-        currency_limit = round(remain_limit / self.get_rate(currency), 2)
+    def get_today_cash_remained(self, currency: str):
+        remain_limit = self.remain_limit()
+        currency_rate, currency_name = self.get_currency(currency)
+        currency_limit = round(remain_limit / currency_rate, 2)
+        if currency_limit == 0:
+            return 'Денег нет, держись'
 
         if currency_limit > 0:
-            return('На сегодня осталось '
-                   f'{currency_limit} '
-                   f'{self.CURRENCY_NAME[currency]}')
+            return f'На сегодня осталось {currency_limit} {currency_name}'
 
-        elif currency_limit < 0:
-            return('Денег нет, держись: твой долг - '
-                   f'{abs(currency_limit)} '
-                   f'{self.CURRENCY_NAME[currency]}')
         else:
-            return 'Денег нет, держись'
+            currency_limit = abs(currency_limit)
+            return f'Денег нет, держись: твой долг - {currency_limit} ' \
+                   f'{currency_name}'
